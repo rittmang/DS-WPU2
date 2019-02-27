f9c78f4b50ec068d32344acf41c7d541486c7828
rittmang <rightonrittman@gmail.com>
2019-02-27 14:41:03 +0530

    Correct changes

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
-using namespace std
+using namespace std;
 
 class Node
 {
+  friend class Polynomial;
   int exp;
   double coeff;
   Node* next;
@@ -15,7 +16,7 @@ public:
     coeff=0.0;
     next=NULL;
   }
-}
+};
 class Polynomial
 {
   Node *H;
@@ -24,16 +25,33 @@ public:
   {
     H=new Node();
     H->coeff=-1;
+    H->next=H;
   }
   void display()
   {
+    Node* current=H;
+    current=current->next;
 
+    while(current!=H)
+    {
+      if(current->next!=H)
+        cout<<current->coeff<<"X^"<<current->exp<<"+";
+
+      else
+        cout<<current->coeff<<"X^"<<current->exp;
+
+      current=current->next;
+    }
+    cout<<"\n\n";
   }
   void create()
   {
     int n=0;
     cout<<"Enter number of nodes";
     cin>>n;
+    Node* current=H;
+    while(current->next!=H)
+      current=current->next;
 
     for(int i=0;i<n;i++)
     {
@@ -43,17 +61,19 @@ public:
         cout<<"Enter exponent:";
         cin>>nn->exp;
         cout<<"------------\n";
-        H->next=nn;
-        nn->next=NULL;
+        current->next=nn;
+        nn->next=H;
+        current=nn;
     }
   }
 
-}
+};
 int main()
 {
-  Polynomial p1=new Polynomial();
+  Polynomial* p1=new Polynomial();
 
-  p1.
+  p1->create();
+  p1->display();
 
   return 0;
 }
