beebaf1246c6de1d679bc7d24d53ef5c828ce2bc
rittmang <rightonrittman@gmail.com>
2019-03-04 20:25:32 +0530

    Edited to insert nodes in ascending order, 1x^1, then 1x^2

--- polynomial.cpp
+++ polynomial.cpp
@@ -25,7 +25,7 @@ public:
   Polynomial()
   {
     H=new Node();
-    H->coeff=-1.0;
+    H->exp=-1;
     H->next=H;
   }
   void display()
@@ -47,20 +47,21 @@ public:
   }
   void insert(int exp1,double coeff1)
   {
-  	Node* current=H;
-   	 while(current->next!=H)
-      		current=current->next;
-  	
-  	
-  	Node* nn=new Node();
-       	nn->coeff=coeff1;
-        nn->exp=exp1;
-        
-        current->next=nn;
-        nn->next=H;
-        //current=nn;
-  	
-  }
+    Node* current=H;
+    Node* nn=new Node();
+    nn->coeff=coeff1;
+    nn->exp=exp1;
+
+    while(current->next!=H && current->next->exp < nn->exp)
+      {current=current->next;}
+
+
+
+    Node *temp=current;
+    current=current->next;
+    temp->next=nn;
+    nn->next=current;
+}
   void evaluate()
   {
     Node* current=H;
@@ -83,22 +84,22 @@ public:
  Polynomial* add(Polynomial *p2)
  {
   	Polynomial* p3=new Polynomial();
-  	
+
   	Node* current1=this->H;
   	current1=current1->next;
-  	
+
   	Node* current2=p2->H;
   	current2=current2->next;
-  	
+
   	while(current1 != this->H && current2 != p2->H)
   	{
-  		
-  		if(current1->exp > current2->exp)
+
+  		if(current1->exp < current2->exp)
   		{
   			p3->insert(current1->exp,current1->coeff);
   			current1=current1->next;
   		}
-  		if(current1->exp < current2->exp)
+  		if(current1->exp > current2->exp)//signs inverted < & >
   		{
   			p3->insert(current2->exp,current2->coeff);
   			current2=current2->next;
@@ -111,11 +112,11 @@ public:
   				current1=current1->next;
   				current2=current2->next;
   			}
-  		}	
-  		
-  		
-  		
-  		
+  		}
+
+
+
+
   	}
   	while(current1!=this->H)
   	{
@@ -127,33 +128,39 @@ public:
   		p3->insert(current2->exp,current2->coeff);
   		current2=current2->next;
   	}
-  	return p3; 	
-  	
- } 
+  	return p3;
+
+ }
 
 
 
- 
+
  void create()
   {
     int n=0;
     cout<<"Enter number of nodes";
     cin>>n;
-    Node* current=H;
-    while(current->next!=H)
-      current=current->next;
+
+    //while(current->next!=H ||   )
+      //current=current->next;
 
     for(int i=0;i<n;i++)
     {
+        Node* current=H;
         Node* nn=new Node();
         cout<<"Enter coefficient:";
         cin>>nn->coeff;
         cout<<"Enter exponent:";
         cin>>nn->exp;
         cout<<"------------\n";
-        current->next=nn;
-        nn->next=H;
-        current=nn;
+
+        while(current->next!=H && current->next->exp < nn->exp)
+          {current=current->next;}
+
+        Node *temp=current;
+        current=current->next;
+        temp->next=nn;
+        nn->next=current;
     }
   }
 
@@ -164,30 +171,30 @@ int main()
   Polynomial* p2=new Polynomial();
   Polynomial* p3=new Polynomial();
   int nopoly,choice;
-  
+
   do
   {
   	cout<<"\nChoose Polynomial(1-2):\n";
   	cin>>nopoly;
   	if(nopoly==0)break;
-  	
+
   	cout<<"******MENU*******\n1. Create Polynomial\n2. Display Polynomial\n3. Add Polynomial(defunct)\n4. Evaluate Polynomial\n";
   	cin>>choice;
-  	
+
   	if(nopoly==1)
   	{
   		switch(choice)
   		{
   			case 1: p1->create();
   				break;
-  			
+
   			case 2: p1->display();
   				break;
-  				
+
   			case 3: p3=p1->add(p2);
   				p3->display();
   				break;
-  			
+
   			case 4: p1->evaluate();
   				break;
   		}
@@ -198,19 +205,19 @@ int main()
   		{
   			case 1: p2->create();
   				break;
-  			
+
   			case 2: p2->display();
   				break;
-  				
+
   			case 3: p3=p2->add(p1);
   				p3->display();
   				break;
-  			
+
   			case 4: p2->evaluate();
   				break;
   		}
   	}
-  	
+
   }while(choice!=0);
   return 0;
 }
