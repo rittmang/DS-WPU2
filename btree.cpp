b03a698afed2468694a27667f6b89178e5e58a95
rittmang <rightonrittman@gmail.com>
2019-03-05 19:52:08 +0530

    Adding code

---
+++ btree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+  int data;
+  Node* left,right;
+
+public:
+  Node()
+  {
+    data=-sizeof(int);
+    left=NULL;
+    right=NULL;
+  }
+}
+class BinaryTree
+{
+  
+}
